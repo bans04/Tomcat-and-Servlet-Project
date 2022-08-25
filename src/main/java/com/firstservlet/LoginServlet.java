84d1208f266a45bb82ced29744f302c925927041
bans04 <bansodednyanadeo@gmail.com>
2022-08-26 00:52:51 +0530

    Added:-> Extend the servlet to accept a valid name. Name start with Cap and has min 3 char

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -28,11 +29,13 @@ public class LoginServlet extends HttpServlet {
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
-        if (userId.equals(user) && password.equals(pwd)) {
+        Boolean userNameValidator = Pattern.matches(("[A-Z][a-z]{3,}"), user);
+
+        if (userNameValidator && userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
             out.println("<font color = red > Either user name or password is wrong.</font>");
             rd.include(request, response);
